8aa82e8a4d86a09c3088fde1399277989d0b2ba1
Shunichi Hirose <shunhiroseluvmri@gmail.com>
2020-04-23 23:34:14 +0900

    お気に入りタスク切り替えボタンの実装(part1)

---
+++ ToDoApp/taskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  taskTableViewCell.swift
+//  ToDoApp
+//
+//  Created by 廣瀬舜一 on 2020/04/22.
+//  Copyright © 2020 廣瀬舜一. All rights reserved.
+//
+
+import UIKit
+
+class taskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
